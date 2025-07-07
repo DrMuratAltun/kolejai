393c6f21ef1b9d2993e7f8ba28a72009ccfe9bfd
YZO Yapay Zeka Okulum <yapayzekaokulum@gmail.com>
2025-07-07 21:24:30 +0000

    Error: Module [project]/src/components/layout/ConditionalLayout.tsx [app

--- src/components/layout/Header.tsx
+++
@@ -1,1 +0,0 @@
-// This file is intentionally left blank. Its contents have been integrated into /src/app/layout.tsx to resolve a module dependency issue.
--- src/lib/data.ts
+++
@@ -1,3 +0,0 @@
-// This file is now empty as news and events are managed via Firebase.
-// You can use this file for other static data in the future.
-export const newsAndEvents: any[] = [];
